ffc0727bbfc10b75c7ffd55f8511b787b984dc69
Andres Rivero <andres19rivero@gmail.com>
2021-08-17 22:22:36 -0400

    feat: my_each_with_index

--- enumerable.rb
+++ enumerable.rb
@@ -1,5 +1,19 @@
 module Enumerable
   def my_each
-    yield
+    cont = 0
+    until cont == size
+      yield self[cont]
+      cont += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    i = 0
+    until i == size
+      yield self[i], i
+      i += 1
+    end
+    self
   end
 end
--- main.rb
+++ main.rb
@@ -7,3 +7,10 @@ puts 'My Each'
 numbers.my_each { |item| puts item }
 puts 'Each'
 numbers.each { |item| puts item }
+
+puts 'my_each_with_index vs. each_with_index'
+numbers = [5, 4, 3, 2, 1]
+puts 'Each with index'
+numbers.each_with_index { |val, index| puts "Value: #{val}, index: #{index}" }
+puts 'My Each with index'
+numbers.my_each_with_index { |val, index| puts "Value: #{val}, index: #{index}" }
